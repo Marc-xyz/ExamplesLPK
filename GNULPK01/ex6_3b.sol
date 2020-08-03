f595e6d05e888af5a43e213520641a82ff98c0db
Marc-xyz <49844756+Marc-xyz@users.noreply.github.com>
2020-08-03 23:00:15 +0200

    Add ex6_3b.sol

---
+++ GNULPK01/ex6_3b.sol
@@ -0,0 +1,43 @@
+Problem:    ex6_3b
+Rows:       7
+Columns:    4
+Non-zeros:  21
+Status:     OPTIMAL
+Objective:  z = -29 (MINimum)
+
+   No.   Row name   St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 z            B            -29                             
+     2 r1           NS            -7            -7             =             3 
+     3 r2           B            148            14               
+     4 r3           B            -13                          -3 
+     5 r4           NS             2             2             =            -4 
+     6 r5           NL             0            -0                           6 
+     7 r6           NL             0            -0                          10 
+
+   No. Column name  St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 x[4]         B             25                             
+     2 x[3]         B              0                             
+     3 x[2]         B              9                             
+     4 x[1]         B              0                             
+
+Karush-Kuhn-Tucker optimality conditions:
+
+KKT.PE: max.abs.err = 1.07e-14 on row 5
+        max.rel.err = 1.94e-16 on row 5
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.DE: max.abs.err = 1.78e-15 on column 3
+        max.rel.err = 7.11e-17 on column 3
+        High quality
+
+KKT.DB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output
