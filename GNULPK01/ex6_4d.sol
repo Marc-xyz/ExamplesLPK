13eae10b57eaf2537dde88c665d97daf75ade8c4
Marc-xyz <49844756+Marc-xyz@users.noreply.github.com>
2020-08-03 23:05:01 +0200

    Add ex6_4d.sol

---
+++ GNULPK01/ex6_4d.sol
@@ -0,0 +1,42 @@
+Problem:    ex6_4d
+Rows:       7
+Columns:    3
+Non-zeros:  14
+Status:     OPTIMAL
+Objective:  z = 3.5 (MAXimum)
+
+   No.   Row name   St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 z            B            3.5                             
+     2 r[1]         NL             0            -0                       < eps
+     3 r[2]         B            0.9            -0               
+     4 r[3]         B            1.7            -0               
+     5 r4           NU             6                           6           0.5 
+     6 r5           B            0.9                           3 
+     7 r6           NU             1                           1           0.5 
+
+   No. Column name  St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 x[3]         B            1.7                             
+     2 x[2]         B            0.9                             
+     3 x[1]         B              0                             
+
+Karush-Kuhn-Tucker optimality conditions:
+
+KKT.PE: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.DE: max.abs.err = 2.22e-16 on column 1
+        max.rel.err = 5.55e-17 on column 1
+        High quality
+
+KKT.DB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output
