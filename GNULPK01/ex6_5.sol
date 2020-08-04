dd46bcd004cf14256bfdb3e21f74428f4970c0f6
Marc-xyz <49844756+Marc-xyz@users.noreply.github.com>
2020-08-04 21:37:56 +0200

    Add ex6_5.sol

---
+++ GNULPK01/ex6_5.sol
@@ -0,0 +1,40 @@
+Problem:    ex6_5
+Rows:       5
+Columns:    3
+Non-zeros:  15
+Status:     OPTIMAL
+Objective:  cost = 2.979166667 (MINimum)
+
+   No.   Row name   St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 cost         B        2.97917                             
+     2 rest_cal1    B          18000         16000               
+     3 rest_cal2    NU         18000                       18000  -0.000385417 
+     4 rest_prot    NL             2             2                    0.583333 
+     5 rest_vit     NL             3             3                     2.91667 
+
+   No. Column name  St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 x[Aliment_1] B       0.104167             0               
+     2 x[Aliment_2] B        1.97917             0               
+     3 x[Aliment_3] B        8.54167             0               
+
+Karush-Kuhn-Tucker optimality conditions:
+
+KKT.PE: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.DE: max.abs.err = 1.11e-16 on column 3
+        max.rel.err = 4.22e-17 on column 3
+        High quality
+
+KKT.DB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output
