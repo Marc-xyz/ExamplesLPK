dd39bedd895014e49053a00c69292efa68a51800
Marc-xyz <49844756+Marc-xyz@users.noreply.github.com>
2020-08-04 21:39:00 +0200

    Add ex6_6a.sol

---
+++ ex6_6a.sol
@@ -0,0 +1,39 @@
+Problem:    ex6_6a
+Rows:       4
+Columns:    3
+Non-zeros:  6
+Status:     OPTIMAL
+Objective:  min_person = 30 (MINimum)
+
+   No.   Row name   St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 min_person   B             30                             
+     2 r1           NL            10            10                           1 
+     3 r2           NL            12            12                           1 
+     4 r3           NL             8             8                           1 
+
+   No. Column name  St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 x[1]         B             10             0               
+     2 x[2]         B             12             0               
+     3 x[3]         B              8             0               
+
+Karush-Kuhn-Tucker optimality conditions:
+
+KKT.PE: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.DE: max.abs.err = 0.00e+00 on column 0
+        max.rel.err = 0.00e+00 on column 0
+        High quality
+
+KKT.DB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output
