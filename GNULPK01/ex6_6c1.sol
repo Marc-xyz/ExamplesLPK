7aa6c303aace3072ff472d35c09fc1b4b86e6850
Marc-xyz <49844756+Marc-xyz@users.noreply.github.com>
2020-08-05 23:47:19 +0200

    Add ex6_6c1.sol

---
+++ GNULPK01/ex6_6c1.sol
@@ -0,0 +1,37 @@
+Problem:    ex6_6c1
+Rows:       7
+Columns:    6 (6 integer, 0 binary)
+Non-zeros:  18
+Status:     INTEGER OPTIMAL
+Objective:  min_person = 26 (MINimum)
+
+   No.   Row name        Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 min_person                 26                             
+     2 r[1]                       12            12               
+     3 r[2]                       10            10               
+     4 r[3]                       10             7               
+     5 r[4]                       12            12               
+     6 r[5]                        4             4               
+     7 r6                          4             4               
+
+   No. Column name       Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 x[1]         *              4             0               
+     2 x[2]         *              8             0               
+     3 x[3]         *              2             0               
+     4 x[4]         *              8             0               
+     5 x[5]         *              4             0               
+     6 x[6]         *              0             0               
+
+Integer feasibility conditions:
+
+KKT.PE: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output
