4d29dd2994e932cfbe77e48d5619801fe1ab4e9a
Marc-xyz <49844756+Marc-xyz@users.noreply.github.com>
2020-08-08 23:55:28 +0200

    Add exx4_2a.sol

---
+++ GNULPK02/exx4_2a.sol
@@ -0,0 +1,57 @@
+Problem:    exx4_2a
+Rows:       10
+Columns:    15 (15 integer, 0 binary)
+Non-zeros:  57
+Status:     INTEGER OPTIMAL
+Objective:  Km_bus = 540 (MINimum)
+
+   No.   Row name        Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 Km_bus                    540                             
+     2 capacitat_centre[1]
+                                  90                          90 
+     3 capacitat_centre[2]
+                                  90                          90 
+     4 capacitat_centre[3]
+                                  70                          90 
+     5 estudiants[A]
+                                  70                          70 
+     6 estudiants[B]
+                                  50                          50 
+     7 estudiants[C]
+                                  10                          10 
+     8 estudiants[D]
+                                  80                          80 
+     9 estudiants[E]
+                                  40                          40 
+    10 omplir                    250           250             = 
+
+   No. Column name       Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 x[1,A]       *             40             0               
+     2 x[1,B]       *             50             0               
+     3 x[1,C]       *              0             0               
+     4 x[1,D]       *              0             0               
+     5 x[1,E]       *              0             0               
+     6 x[2,A]       *              0             0               
+     7 x[2,B]       *              0             0               
+     8 x[2,C]       *             10             0               
+     9 x[2,D]       *             80             0               
+    10 x[2,E]       *              0             0               
+    11 x[3,A]       *             30             0               
+    12 x[3,B]       *              0             0               
+    13 x[3,C]       *              0             0               
+    14 x[3,D]       *              0             0               
+    15 x[3,E]       *             40             0               
+
+Integer feasibility conditions:
+
+KKT.PE: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output
