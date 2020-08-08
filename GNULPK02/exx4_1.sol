1125b65f399a73cda656619e3803849a5699c98f
Marc-xyz <49844756+Marc-xyz@users.noreply.github.com>
2020-08-08 23:52:10 +0200

    Add exx4_1.sol

---
+++ GNULPK02/exx4_1.sol
@@ -0,0 +1,36 @@
+Problem:    exx4_1
+Rows:       2
+Columns:    10 (10 integer, 10 binary)
+Non-zeros:  20
+Status:     INTEGER OPTIMAL
+Objective:  z = 200 (MAXimum)
+
+   No.   Row name        Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 z                         200                             
+     2 r_1                       100                         100 
+
+   No. Column name       Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 x[O_1]       *              0             0             1 
+     2 x[O_2]       *              0             0             1 
+     3 x[O_3]       *              0             0             1 
+     4 x[O_4]       *              1             0             1 
+     5 x[O_5]       *              1             0             1 
+     6 x[O_6]       *              1             0             1 
+     7 x[O_7]       *              0             0             1 
+     8 x[O_8]       *              0             0             1 
+     9 x[O_9]       *              0             0             1 
+    10 x[O_10]      *              1             0             1 
+
+Integer feasibility conditions:
+
+KKT.PE: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output
