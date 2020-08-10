07d229d747a8b5e51b84ae7df3945b03bdbc6aa3
Marc-xyz <49844756+Marc-xyz@users.noreply.github.com>
2020-08-10 23:51:36 +0200

    Add ./exx4_5.sol

---
+++ GNULPK02/exx4_5.sol
@@ -0,0 +1,38 @@
+Problem:    exx4_5
+Rows:       7
+Columns:    7 (7 integer, 7 binary)
+Non-zeros:  25
+Status:     INTEGER OPTIMAL
+Objective:  numer_cent = 2 (MINimum)
+
+   No.   Row name        Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 numer_cent                  2                             
+     2 Minim[D_1]                  2             1               
+     3 Minim[D_2]                  1             1               
+     4 Minim[D_3]                  1             1               
+     5 Minim[D_4]                  1             1               
+     6 Minim[D_5]                  1             1               
+     7 Minim[D_6]                  1             1               
+
+   No. Column name       Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 x[C_1]       *              0             0             1 
+     2 x[C_2]       *              0             0             1 
+     3 x[C_3]       *              1             0             1 
+     4 x[C_4]       *              0             0             1 
+     5 x[C_5]       *              1             0             1 
+     6 x[C_6]       *              0             0             1 
+     7 x[C_7]       *              0             0             1 
+
+Integer feasibility conditions:
+
+KKT.PE: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output
