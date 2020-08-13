85b25ffa776ef862b5d3695476d30a752ebc2c80
Marc-xyz <49844756+Marc-xyz@users.noreply.github.com>
2020-08-13 23:52:03 +0200

    Add ./exx4_6b.sol

---
+++ GNULPK02/exx4_6b.sol
@@ -0,0 +1,89 @@
+Problem:    exx4_6b
+Rows:       23
+Columns:    21 (21 integer, 0 binary)
+Non-zeros:  126
+Status:     INTEGER OPTIMAL
+Objective:  treballadors = 33 (MINimum)
+
+   No.   Row name        Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 treballadors               33                             
+     2 a_sindicat[nit,1]
+                                   4             4               
+     3 a_sindicat[nit,2]
+                                   3             3               
+     4 a_sindicat[nit,3]
+                                   3             2               
+     5 a_sindicat[nit,4]
+                                   3             2               
+     6 a_sindicat[mati,1]
+                                   7             5               
+     7 a_sindicat[mati,2]
+                                   7             7               
+     8 a_sindicat[mati,3]
+                                   4             2               
+     9 a_sindicat[mati,4]
+                                   5             5               
+    10 a_sindicat[tarda,1]
+                                  10             7               
+    11 a_sindicat[tarda,2]
+                                  11            11               
+    12 a_sindicat[tarda,3]
+                                   5             2               
+    13 a_sindicat[tarda,4]
+                                   5             2               
+    14 b_sindicat[nit,1]
+                                   5             5               
+    15 b_sindicat[nit,2]
+                                   3             3               
+    16 b_sindicat[nit,3]
+                                   3             2               
+    17 b_sindicat[mati,1]
+                                   8             7               
+    18 b_sindicat[mati,2]
+                                   8             8               
+    19 b_sindicat[mati,3]
+                                   9             9               
+    20 b_sindicat[tarda,1]
+                                   9             9               
+    21 b_sindicat[tarda,2]
+                                  10            10               
+    22 b_sindicat[tarda,3]
+                                  10            10               
+    23 treball                    33                          60 
+
+   No. Column name       Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 tr[nit,1]    *              3             0               
+     2 tr[nit,2]    *              0             0               
+     3 tr[nit,3]    *              0             0               
+     4 tr[nit,4]    *              1             0               
+     5 tr[nit,5]    *              2             0               
+     6 tr[nit,6]    *              0             0               
+     7 tr[nit,7]    *              0             0               
+     8 tr[mati,1]   *              3             0               
+     9 tr[mati,2]   *              3             0               
+    10 tr[mati,3]   *              1             0               
+    11 tr[mati,4]   *              0             0               
+    12 tr[mati,5]   *              3             0               
+    13 tr[mati,6]   *              0             0               
+    14 tr[mati,7]   *              2             0               
+    15 tr[tarda,1]  *              4             0               
+    16 tr[tarda,2]  *              6             0               
+    17 tr[tarda,3]  *              0             0               
+    18 tr[tarda,4]  *              0             0               
+    19 tr[tarda,5]  *              5             0               
+    20 tr[tarda,6]  *              0             0               
+    21 tr[tarda,7]  *              0             0               
+
+Integer feasibility conditions:
+
+KKT.PE: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output
